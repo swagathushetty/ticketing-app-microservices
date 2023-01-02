d82f95fb9d706bf1ad3f03d2d59f0f4301b9f8f2
swagath shetty <swagath.shetty@gmail.com>
2023-01-02 09:46:15 +0530

    done with k8s for auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,11 @@ import {json} from 'body-parser'
 const app=express()
 app.use(json())
 
-
+console.log('entered API........')
+app.get('/api/users/currentusers',(req,res)=>{
+    console.log('hit the api')
+    res.send('Hi there')
+})
 app.listen(3000,()=>{
     console.log('listening on port 3000')
 })
