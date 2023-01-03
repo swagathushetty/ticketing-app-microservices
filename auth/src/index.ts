2bb004f45a9bfcebc3d44160603868a08c3170b6
swagath shetty <swagath.shetty@gmail.com>
2023-01-03 14:21:49 +0530

    master cleaning up routing structure

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,14 +1,10 @@
 import express from 'express'
 import {json} from 'body-parser'
-
+import { currentUserRouter } from './routes/current-user'
 const app=express()
 app.use(json())
 
-console.log('entered API........')
-app.get('/api/users/currentusers',(req,res)=>{
-    console.log('hit the api')
-    res.send('Hi there')
-})
+app.use(currentUserRouter)
 app.listen(3000,()=>{
     console.log('listening on port 3000')
 })
