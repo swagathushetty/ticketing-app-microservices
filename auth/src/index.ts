2b60a9225c86c878f821be650b97cabc3c4c234e
swagath shetty <swagath.shetty@gmail.com>
2023-01-09 07:40:18 +0530

    did basic route scaffolding

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express'
 import {json} from 'body-parser'
 import { currentUserRouter } from './routes/current-user'
+import { signoutRouter } from './routes/signout'
+import { siginRouter } from './routes/signin'
+import { signupRouter } from './routes/signup'
 const app=express()
 app.use(json())
 
 app.use(currentUserRouter)
+app.use(signoutRouter)
+app.use(siginRouter)
+app.use(signupRouter)
+
 app.listen(3000,()=>{
     console.log('listening on port 3000')
 })
