c61ca89955e6c44df784d147f2c0c5706cf5dc41
swagath shetty <swagath.shetty@gmail.com>
2023-01-15 18:26:13 +0530

    added validation of input

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,16 @@
-import express from 'express'
-
+import express,{Request,Response} from 'express'
+import { body } from 'express-validator'
 const router=express.Router()
 
-router.post('/api/users/signup',(req,res)=>{
+router.post('/api/users/signup',[
+    body('email').isEmail()
+    .withMessage('email must be valid'),
+    body('password')
+    .trim()
+    .isLength({min:4,max:20})
+    .withMessage('password must be between 4 and 20 chars')
+],(req:Request,res:Response)=>{
+    const {email ,password}=req.body
     res.send('hi there')
 })
 
