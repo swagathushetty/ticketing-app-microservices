295506448ba4e278a1e993863e302f48ea08257e
swagath shetty <swagath.shetty@gmail.com>
2023-01-30 07:46:58 +0530

    fixed validator issue

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,21 +1,31 @@
-import express,{Request,Response} from 'express'
-import { body , validationResult} from 'express-validator'
-const router=express.Router()
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
-router.post('/api/users/signup',[
-    body('email').isEmail()
-    .withMessage('email must be valid'),
-    body('password')
-    .trim()
-    .isLength({min:4,max:20})
-    .withMessage('password must be between 4 and 20 chars')
-],(req:Request,res:Response)=>{
-    const errors=validationResult(req)
-    if(!errors.isEmpty){
-        return res.status(400).send(errors.array())
+const router = express.Router();
+
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
     }
-    const {email ,password}=req.body
-    res.send('hi there')
-})
+    const { email, password } = req.body;
 
-export  {router as signupRouter}
+    console.log("Creating a user...");
+
+    res.send({});
+
+    // new User({ email, password })
+  }
+);
+
+export { router as signupRouter };
