0167179adcd270fd9383d6376097e971d220d8f2
swagath shetty <swagath.shetty@gmail.com>
2023-03-01 09:09:33 +0530

    error handler changes

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,7 +1,16 @@
 import { Request,Response,NextFunction } from "express"
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 export const errorHandler = (err:Error,req:Request,res:Response,next:NextFunction)=>{
- console.log('something went wrong',err)
+
+   if(err instanceof RequestValidationError){
+      console.log('handling this error as request validation errror')
+   }
+
+   if(err instanceof DatabaseConnectionError){
+      console.log('handling error as DB connection error')
+   }
 
  res.status(400).send({
     message:err.message
