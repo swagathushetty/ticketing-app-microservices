b07fefc7cbca1e621eafbb732a165e58b6d98c92
swagath shetty <swagath.shetty@gmail.com>
2023-05-29 08:28:59 +0530

    added cookie session package

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,15 +1,25 @@
 import express from 'express';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
-
+import cookieSession from 'cookie-session';
 import { currentUserRouter } from './routes/current-user';
 import { siginRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-errors';
+
 const app = express();
+
+
+app.set('trust proxy',true)
+
 app.use(json());
+
+app.use(cookieSession({
+  signed:false,
+  secure:true
+}))
 
 app.use(currentUserRouter);
 app.use(siginRouter);
