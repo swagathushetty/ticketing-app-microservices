8914d93df0f288e16f408e61fa111d43ada137b8
swagath shetty <swagath.shetty@gmail.com>
2023-06-25 17:30:22 +0530

    added utlity middlewares

---
+++ auth/src/errors/not-authorized-error.ts
@@ -0,0 +1,18 @@
+import { CustomError } from "./custom-errors"
+
+export class NotAuthorizedError extends CustomError {
+    statusCode = 401
+    constructor(){
+        super('Not authorized')
+
+        Object.setPrototypeOf(this,NotAuthorizedError.prototype)
+    }
+
+
+
+    seralizeErrors(){
+        return [
+            {'message':'Not Authorised'}
+        ]
+    }
+}
